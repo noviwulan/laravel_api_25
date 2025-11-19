206718573ab94e0c829622bf4aed1133a093b38d
noviwulan <novid4691@gmail.com>
2025-11-18 23:51:31 -0600

    postman

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -12,11 +12,10 @@ Route::prefix('v1')->group(function(){
     Route::resource('vendor',VendorController::class);
 });
 
-<<<<<<< HEAD
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-=======
 
 Route::get('/prducts', [ProductController::class, 'index']);
 Route::post('/prducts', [ProductController::class, 'store']);
@@ -25,4 +24,3 @@ Route::resource('vendors', VendorController::class);
 Route::get('/Halo', function (){
     return 'Halo, Laravel';
 });
->>>>>>> 5a7cb1f8631d8f1e5018897e3e4cd6c4d8087ca1
